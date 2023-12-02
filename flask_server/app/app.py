2753f71cc4a884915d7c4191e841d1b54370ab9f
Pranav <Pranav@rawsvr-db.rehabatwork.com>
2023-12-02 17:58:02 -0500

    fixed more links

--- flask_server/app/app.py
+++ flask_server/app/app.py
@@ -3,139 +3,98 @@ import pyodbc
 
 app = Flask(__name__)
 
-#RAWSVR-DB\ADA
-#rawsvr-db.rehabatwork.com
 # Database configuration
-server = 'RAWSVR-DB\ADA'  # e.g., 'localhost\instancename' or 'myserver.database.windows.net'
-database = 'PatientForm'  # e.g., 'mydatabase'
-username = 'sa'  # e.g., 'username'
-password = '$Monday01'  # e.g., 'password'
-driver = '{ODBC Driver 18 for SQL Server}'  # e.g., '{SQL Server}' - depending on your ODBC driver's name
+server = r'RAWSVR-DB\ADA'  # Use a raw string to handle the backslash
+database = 'PatientForm'
+username = 'sa'
+password = '$Monday01'
+driver = '{ODBC Driver 18 for SQL Server}'
 
 # Establish a database connection and return cursor
 def get_db_connection():
     conn = pyodbc.connect(
         'DRIVER=' + driver + ';SERVER=' + server +
         ';DATABASE=' + database + ';UID=' + username +
-        ';PWD=' + password)
+        ';PWD=' + password + ';TrustServerCertificate=Yes')
     return conn
 
 @app.route('/')
 def index():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM your_table')  # Replace 'your_table' with your actual table name
+    cursor.execute('SELECT * FROM your_table')  # Replace with your actual table name
     data = cursor.fetchall()
-    # Transform query results into a list of dictionaries (or any format you prefer)
-    result = [{'column1': row[0], 'column2': row[1]} for row in data]  # Edit column names as appropriate
+    result = [{'column1': row[0], 'column2': row[1]} for row in data]  # Adjust as per your table structure
     cursor.close()
     conn.close()
     return jsonify(result)
 
-# Route to fetch dead referral reasons
 @app.route('/dead_referral_reasons')
 def get_dead_referral_reasons():
     conn = get_db_connection()
     cursor = conn.cursor()
-
-    # Select only the 'label' column from the 'dead-referral-reasons' table
-    cursor.execute('SELECT label FROM dead_referral_reasons')  # Ensure the table name is correctly referenced
+    cursor.execute('SELECT label FROM dead_referral_reasons')  # Replace with actual table name
     referrals = cursor.fetchall()
-    print(referrals)
-
-    # Extract the 'label' values into a list of strings
-    options = [row['label'] for row in referrals]  # Adjust based on your actual result structure
+    options = [row[0] for row in referrals]  # Accessing by index
     options.append('Other')
-    
     cursor.close()
     conn.close()
     return jsonify(options)
 
-# Route to fetch attorney claimants
 @app.route('/attorney_claimant')
 def get_attorney_claimant():
     conn = get_db_connection()
     cursor = conn.cursor()
-
-    # Select only the 'label' column from the 'dead-referral-reasons' table
-    cursor.execute('SELECT Full_Name FROM attorneys')  # Ensure the table name is correctly referenced
+    cursor.execute('SELECT Full_Name FROM attorneys')  # Replace with actual table name
     attorneys = cursor.fetchall()
-
-    # Extract the 'label' values into a list of strings
-    options = [row['Full_Name'] for row in attorneys]  # Adjust based on your actual result structure
-    
+    options = [row[0] for row in attorneys]  # Accessing by index
     cursor.close()
     conn.close()
     return jsonify(options)
 
-# Route to fetch billed parties
 @app.route('/billed_party')
 def get_billed_party():
     conn = get_db_connection()
     cursor = conn.cursor()
-
-    # Select only the 'label' column from the 'dead-referral-reasons' table
-    cursor.execute('SELECT Name FROM billed_party')  # Ensure the table name is correctly referenced
+    cursor.execute('SELECT Name FROM billed_party')  # Replace with actual table name
     billed = cursor.fetchall()
-
-    # Extract the 'label' values into a list of strings
-    options = [row['Name'] for row in billed]  # Adjust based on your actual result structure
-    
+    options = [row[0] for row in billed]  # Accessing by index
     cursor.close()
     conn.close()
     return jsonify(options)
 
-# Route to fetch billed parties
 @app.route('/case-managers')
 def get_managers():
     conn = get_db_connection()
     cursor = conn.cursor()
-
-    # Select only the 'label' column from the 'dead-referral-reasons' table
-    cursor.execute('SELECT Full_Name FROM case_managers')  # Ensure the table name is correctly referenced
+    cursor.execute('SELECT Full_Name FROM case_managers')  # Replace with actual table name
     case_managers = cursor.fetchall()
-
-    # Extract the 'label' values into a list of strings
-    options = [row['Full_Name'] for row in case_managers]  # Adjust based on your actual result structure
-    
+    options = [row[0] for row in case_managers]  # Accessing by index
     cursor.close()
     conn.close()
     return jsonify(options)
 
-# Route to fetch billed parties
 @app.route('/employers')
 def get_employers():
     conn = get_db_connection()
     cursor = conn.cursor()
-
-    # Select only the 'label' column from the 'dead-referral-reasons' table
-    cursor.execute('SELECT Name FROM employer_dropdown')  # Ensure the table name is correctly referenced
+    cursor.execute('SELECT Name FROM employers')  # Replace with actual table name
     employers = cursor.fetchall()
-
-    # Extract the 'label' values into a list of strings
-    options = [row['Name'] for row in employers]  # Adjust based on your actual result structure
-    
+    options = [row[0] for row in employers]  # Accessing by index
     cursor.close()
     conn.close()
     return jsonify(options)
 
-# Route to fetch billed parties
 @app.route('/practitioners')
 def get_practitioners():
     conn = get_db_connection()
     cursor = conn.cursor()
-
-    # Select only the 'label' column from the 'dead-referral-reasons' table
-    cursor.execute('SELECT Full_Name FROM practitioners')  # Ensure the table name is correctly referenced
+    cursor.execute('SELECT Full_Name FROM practitioners')  # Replace with actual table name
     practitioners = cursor.fetchall()
-
-    # Extract the 'label' values into a list of strings
-    options = [row['Full_Name'] for row in practitioners]  # Adjust based on your actual result structure
-    
+    options = [row[0] for row in practitioners]  # Accessing by index
     cursor.close()
     conn.close()
     return jsonify(options)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
