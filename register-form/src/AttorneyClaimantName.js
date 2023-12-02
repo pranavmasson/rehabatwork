98d8a8706039f5aa6e5d7e3b6876eb879bcb3556
pranav masson <pranavmasson@pranavs-MacBook-Air.local>
2023-12-02 17:52:29 -0500

    fixed flask links

--- register-form/src/AttorneyClaimantName.js
+++ register-form/src/AttorneyClaimantName.js
@@ -7,7 +7,7 @@ const AttorneyClaimantName = ({ name, value, onChange }) => {
   useEffect(() => {
     // Fetch data from the database or an API endpoint.
     // Replace the fetch URL with your actual backend endpoint.
-    fetch('https://your-api-endpoint.com/data')
+    fetch('http://127.0.0.1:5000/attorney_claimant')
       .then((response) => response.json())
       .then((data) => {
         // Assuming data is an array of objects with 'value' and 'label' properties.
